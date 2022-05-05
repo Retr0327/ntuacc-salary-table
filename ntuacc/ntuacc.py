5749577d1769ef538cf9449cf9d8b5bbdf1c4e54
Retr0327 <philcoke35@gmail.com>
2022-05-05 19:34:40 +0800

    Validate query

--- ntuacc/ntuacc.py
+++ ntuacc/ntuacc.py
@@ -1,6 +1,20 @@
 from .util import convert
 from .core import SalaryTable
 from dataclasses import dataclass
+from pydantic import StrictStr, StrictInt, BaseModel
+
+
+class QueryInfo(BaseModel):
+    """
+    The QueryInfo object keeps tract of the query in inventory, including bossid, assid, asspwd, and project_year
+    """
+
+    campno = "m"
+    idtype = "3"
+    bossid: StrictStr
+    assid: StrictStr
+    asspwd: StrictStr
+    project_year: StrictInt
 
 
 @dataclass
@@ -15,13 +29,12 @@ class NTUACC:
     project_year: int
 
     def __post_init__(self) -> None:
-        self.query = {
-            "campno": "m",
-            "idtype": "3",
-            "bossid": self.bossid,
-            "assid": self.assid,
-            "asspwd": self.asspwd,
-        }
+        self.query = QueryInfo(
+            bossid=self.bossid,
+            assid=self.assid,
+            asspwd=self.asspwd,
+            project_year=self.project_year,
+        ).dict()
 
     def download_table(self) -> SalaryTable:
         """The download_table method downloads the salary table."""
