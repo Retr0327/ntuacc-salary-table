da10ae6d44b14ea3d7da5f0b8372b44697bc77e9
Retr0327 <philcoke35@gmail.com>
2022-05-05 19:51:51 +0800

    Use pydantic dataclass

--- ntuacc/ntuacc.py
+++ ntuacc/ntuacc.py
@@ -1,20 +1,7 @@
 from .util import convert
 from .core import SalaryTable
-from dataclasses import dataclass
-from pydantic import StrictStr, StrictInt, BaseModel
-
-
-class QueryInfo(BaseModel):
-    """
-    The QueryInfo object keeps tract of the query in inventory, including bossid, assid, asspwd, and project_year
-    """
-
-    campno = "m"
-    idtype = "3"
-    bossid: StrictStr
-    assid: StrictStr
-    asspwd: StrictStr
-    project_year: StrictInt
+from pydantic import StrictStr, StrictInt
+from pydantic.dataclasses import dataclass
 
 
 @dataclass
@@ -23,18 +10,19 @@ class NTUACC:
     The NTUACC object downloads the table based on `bossid`, `assid`, `asspwd` and `project_year`.
     """
 
-    bossid: str
-    assid: str
-    asspwd: str
-    project_year: int
+    bossid: StrictStr
+    assid: StrictStr
+    asspwd: StrictStr
+    project_year: StrictInt
 
-    def __post_init__(self) -> None:
-        self.query = QueryInfo(
-            bossid=self.bossid,
-            assid=self.assid,
-            asspwd=self.asspwd,
-            project_year=self.project_year,
-        ).dict()
+    def __post_init__(self):
+        self.query = {
+            "campno": "m",
+            "idtype": "3",
+            "bossid": self.bossid.strip(),
+            "assid": self.assid.strip(),
+            "asspwd": self.asspwd.strip(),
+        }
 
     def download_table(self) -> SalaryTable:
         """The download_table method downloads the salary table."""
