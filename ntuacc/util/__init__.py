65a163379ad2af745ce9a2adac24b7099c7dda11
Retr0327 <philcoke35@gmail.com>
2022-05-06 16:21:18 +0800

    Add download_html

--- ntuacc/util/__init__.py
+++ ntuacc/util/__init__.py
@@ -1,3 +1,3 @@
 from .login import login
 from .converter import convert
-from .utils import set_project_year
+from .utils import set_project_year, download_html
--- ntuacc/util/utils.py
+++ ntuacc/util/utils.py
@@ -1,6 +1,36 @@
+from requests import Session
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+
 def set_project_year(project_year: int):
     if (len(str(project_year))) == 4:
         project_year -= 1911
         return project_year
 
     return project_year
+
+
+def download_html(
+    method: str,
+    session: Session,
+    project_id: str,
+    payload: dict = None,
+) -> BeautifulSoup:
+    HEADERS = {"user-agent": UserAgent().google}
+
+    method_factories = {
+        "post": session.post(
+            "https://ntuacc.cc.ntu.edu.tw/acc/apply/ProjectDetail.asp",
+            data=payload,
+            headers=HEADERS,
+        ),
+        "get": session.get(
+            f"https://ntuacc.cc.ntu.edu.tw/trace/trace.asp?target=self&APPCODE={project_id}&VYEAR={project_id[:3]}",
+            headers=HEADERS,
+        ),
+    }
+
+    html_body = method_factories[method]
+    html_body.encoding = "big5"
+    return BeautifulSoup(html_body.text, "lxml")
