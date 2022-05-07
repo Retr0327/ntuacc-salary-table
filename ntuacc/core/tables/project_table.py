ca0dc169beb9ef2588f32f967deb19b4c10a7239
Retr0327 <philcoke35@gmail.com>
2022-05-07 16:48:31 +0800

    Add `download_tabel_tags` mehtod

--- ntuacc/core/tables/project_table.py
+++ ntuacc/core/tables/project_table.py
@@ -1,6 +1,7 @@
 import pandas as pd
+from typing import Union
 from bs4 import ResultSet
-from ...base import SalaryData
+from ..data import ProjectData
 from dataclasses import dataclass
 
 
@@ -10,7 +11,15 @@ class ProjectTable:
     The ProjectTable object extracts the project dataframe.
     """
 
-    table_tag: ResultSet
+    project_year: int
+
+    def download_tabel_tags(self) -> Union[str, ResultSet]:
+        """The download_tabel_tags method downloads the table tags from ProjectData.
+
+        Returns:
+            a str if there is no project data, a soup object otherwise.
+        """
+        return ProjectData(self.project_year).extract_data()
 
     def format_table(self, data_frame: pd.DataFrame):
         """The format_table method formats the DataFrame
@@ -29,12 +38,17 @@ class ProjectTable:
         table = table.rename(columns={"申請日請": "申請日期"})
         return table
 
-    def build_table(self) -> pd.DataFrame:
+    def build_table(self) -> Union[str, pd.DataFrame]:
         """The build_table method builds the pd.DataFrame based on `table_tag`.
 
         Returns:
             a pd.DataFrame object
         """
 
-        data_frame = pd.read_html(str(self.table_tag))[0]
+        table_tags = self.download_tabel_tags()
+
+        if isinstance(table_tags, str):
+            return table_tags
+
+        data_frame = pd.read_html(str(table_tags))[0]
         return self.format_table(data_frame)
