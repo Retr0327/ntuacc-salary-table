54e01c7e389d902b8b4b60f44ad806aef414a1c2
Retr0327 <philcoke35@gmail.com>
2022-05-07 15:07:48 +0800

    Add format_table

--- ntuacc/util/__init__.py
+++ ntuacc/util/__init__.py
@@ -1,5 +1,5 @@
 from .login import login
 from .converter import convert
-from .utils import set_project_year, download_html
+from .utils import set_project_year, format_table
 
-__all__ = ["login", "convert", "set_project_year", "download_html"]
+__all__ = ["login", "convert", "set_project_year", "format_table"]
