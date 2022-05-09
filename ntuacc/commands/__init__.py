6eceee4fd92d23169a9244799510c7588e6bee9d
Retr0327 <philcoke35@gmail.com>
2022-05-09 15:40:14 +0800

    Add option parser

---
+++ ntuacc/commands/__init__.py
@@ -0,0 +1,3 @@
+from .options import NTUACCParser
+
+__all__ = ["NTUACCParser"]
---
+++ ntuacc/commands/options.py
@@ -0,0 +1,42 @@
+import argparse
+
+
+class NTUACCParser:
+    def __init__(self) -> None:
+        self.parser = argparse.ArgumentParser(
+            prog="NTUACC Salary Table", description="Manage NTUACC salary table log"
+        )
+
+        self.subparser = self.parser.add_subparsers(
+            dest="subcmd", help="description", metavar="Actions", required=True
+        )
+
+        self.add_show()
+
+    def add_show(self):
+        show = self.subparser.add_parser("show", help="Show salary table")
+        show.add_argument(
+            "--bossid", "-B", type=str, required=True, help="Text for boss id"
+        )
+        show.add_argument(
+            "--assid", "-AID", type=str, required=True, help="Text for assistant id"
+        )
+        show.add_argument(
+            "--asspwd",
+            "-AP",
+            type=str,
+            required=True,
+            help="Text for assistant password",
+        )
+        show.add_argument(
+            "--year", "-Y", type=int, required=True, help="Int for project year"
+        )
+
+    def parse_args(self):
+        return self.parser.parse_args()
+
+
+if __name__ == "__main__":
+    ntuacc_parser = NTUACCParser()
+    res = ntuacc_parser.parse_args()
+    print(res)
